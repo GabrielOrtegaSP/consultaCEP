e9a374f93dc60936d634c56c33d724c66720ab46
Gabriel Ortega <42451242+gortegaaa@users.noreply.github.com>
2021-08-12 13:39:17 -0300

    Update consultaCEP.php

--- consultaCEP.php
+++ consultaCEP.php
@@ -3,10 +3,9 @@ $CEP = (isset($_GET['CEP']) and !empty($_GET['CEP']))?$_GET['CEP']:'12236380';
 $ch = curl_init('viacep.com.br/ws/'.$CEP.'/json/');    
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");   
         curl_setopt($ch, CURLOPT_HTTPHEADER, array(     
-            'Content-Type: application/json',         
-            'Content-Length: ' . strlen($data_string))  
+            'Content-Type: application/json'
         );                                            
-        $result = curl_exec($ch);
-        echo $result;
-
+$result = curl_exec($ch);
+$arrayCEP = json_decode($result);
+$print_r($arrayCEP);
 ?>
